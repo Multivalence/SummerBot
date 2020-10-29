51c5424ec867007622d17e15b09e6010351f88b4
Multivalence <47169252+Multivalence@users.noreply.github.com>
2020-10-29 08:34:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import discord
+import json
 import os
 from discord.ext import commands
 
@@ -10,4 +11,9 @@ for filename in os.listdir('./model'):
         client.load_extension(f'model.{filename[:-3]}')
 
 
-client.run('TOKEN')
+
+with open('setup.json') as jsonFile:
+    setup = json.load(jsonFile)
+    token = setup['token']
+
+client.run(token)
