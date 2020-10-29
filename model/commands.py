3a5ddb32cc13f1299b60d3d94b1b3ca993d37582
Multivalence <47169252+Multivalence@users.noreply.github.com>
2020-10-29 01:25:33 -0400

    Create commands.py

---
+++ model/commands.py
@@ -0,0 +1,47 @@
+import discord
+import random
+import json
+from discord.ext import commands
+
+class Commands(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.limited = []
+        self.count = 0
+
+    @commands.command()
+    async def random(self,ctx):
+
+        with open('./model/utils/elements.json', 'r') as jsonFile:
+            data = json.load(jsonFile)
+
+        if len(data) == 0:
+            await ctx.channel.send("No Images or Videos found!")
+            return
+
+
+        image = random.choice(data)
+
+        while image in self.limited:
+            self.count += 1
+
+            if self.count == 20:
+                self.limited = []
+                self.count = 0
+
+            image = random.choice(data)
+
+
+        self.limited.append(image)
+        await ctx.channel.send(image)
+
+
+
+
+
+#Setup
+def setup(client):
+    client.add_cog(Commands(client))
+
+
